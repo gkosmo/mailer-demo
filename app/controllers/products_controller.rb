b9e4938817be48d08f7fcb03feb1393e487d9258
g <gkosmo1@hotmail.com>
2019-08-22 19:38:29 +0200

    testing travis

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,20 @@ class ProductsController < ApplicationController
   def index
         @products = Product.all
   end
+
+  def new
+      @product = Product.new
+  end
+
+  def create
+
+    Product.new(pro_params).save
+    redirect_to root_path
+  end
+
+  private
+
+  def pro_params
+    params.require(:product).permit(:name, :tagline)
+  end
 end
